e20e63d1e19013cac3afffc53f2abf25f2cc2653
AmDe2k <64448840+AmDe2k@users.noreply.github.com>
2020-11-03 15:48:19 -0700

    AD TEst Comment
    Test Comment

--- QA/main.py
+++ QA/main.py
@@ -11,6 +11,7 @@ TokenizedQuestions = {}
 
 Answers = {}
 
+#this is a change I made
 # Gets the data from given Directory and puts the parsed data in dictionaries
 # TODO: make this universal but currently it is easier to do it like this in pycharm
 class GetData:
